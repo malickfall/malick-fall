ad57810a44adeb62f3f10a10365431d6ebd91ea4
mfall <mfall@ubuntu-17.10.1>
2018-11-22 18:08:29 +0100

    add fixtures

--- src/MF/CVBundle/DataFixtures/ORM/LoadCompetenceData.php
+++ src/MF/CVBundle/DataFixtures/ORM/LoadCompetenceData.php
@@ -22,9 +22,14 @@ class LoadCompetenceData extends AbstractFixture implements OrderedFixtureInterf
     public function loadCategories(ObjectManager $manager)
     {
     	$category = new Category();
-		$category->setNom('Techniques');
+		$category->setNom('Backend');
 		$manager->persist($category);
-		$this->addReference('tech', $category);
+		$this->addReference('back', $category);
+
+    	$category3 = new Category();
+		$category3->setNom('Frontend');
+		$manager->persist($category3);
+		$this->addReference('front', $category3);
 
 		$category1 = new Category();
 		$category1->setNom('Gestion de projet');
@@ -36,6 +41,11 @@ class LoadCompetenceData extends AbstractFixture implements OrderedFixtureInterf
     	$manager->persist($category2);
     	$this->addReference('soft', $category2);
 
+		$category4 = new Category();
+		$category4->setNom('Outils');
+    	$manager->persist($category4);
+    	$this->addReference('tools', $category4);
+
         $manager->flush();
     }
 
@@ -44,73 +54,73 @@ class LoadCompetenceData extends AbstractFixture implements OrderedFixtureInterf
     	$competence = new Competence();
 		$competence->setNom('PHP5');
 		$competence->setNiveau(4);
-		$competence->setCategory($this->getReference('tech'));
+		$competence->setCategory($this->getReference('back'));
 		$manager->persist($competence);
 
 		$competence1 = new Competence();
 		$competence1->setNom('Drupal');
 		$competence1->setNiveau(4);
-		$competence1->setCategory($this->getReference('tech'));
+		$competence1->setCategory($this->getReference('tools'));
 		$manager->persist($competence1);
 
 		$competence2 = new Competence();
 		$competence2->setNom('Symfony2');
-		$competence2->setNiveau(3);
-		$competence2->setCategory($this->getReference('tech'));
+		$competence2->setNiveau(4);
+		$competence2->setCategory($this->getReference('back'));
 		$manager->persist($competence2);
 
 		$competence3 = new Competence();
 		$competence3->setNom('GIT');
 		$competence3->setNiveau(4);
-		$competence3->setCategory($this->getReference('tech'));
+		$competence3->setCategory($this->getReference('tools'));
 		$manager->persist($competence3);
 
 		$competence4 = new Competence();
 		$competence4->setNom('Linux');
 		$competence4->setNiveau(3);
-		$competence4->setCategory($this->getReference('tech'));
+		$competence4->setCategory($this->getReference('back'));
 		$manager->persist($competence4);
 
 		$competence5 = new Competence();
 		$competence5->setNom('Javascript');
-		$competence5->setNiveau(3);
-		$competence5->setCategory($this->getReference('tech'));
+		$competence5->setNiveau(4);
+		$competence5->setCategory($this->getReference('back'));
 		$manager->persist($competence5);
 
 		$competence6 = new Competence();
 		$competence6->setNom('JQuery');
 		$competence6->setNiveau(4);
-		$competence6->setCategory($this->getReference('tech'));
+		$competence6->setCategory($this->getReference('front'));
 		$manager->persist($competence6);
 
 		$competence7 = new Competence();
 		$competence7->setNom('Ajax');
 		$competence7->setNiveau(4);
-		$competence7->setCategory($this->getReference('tech'));
+		$competence7->setCategory($this->getReference('back'));
 		$manager->persist($competence7);
 
 		$competence8 = new Competence();
 		$competence8->setNom('PHPUnit');
 		$competence8->setNiveau(3);
-		$competence8->setCategory($this->getReference('tech'));
+		$competence8->setCategory($this->getReference('back'));
 		$manager->persist($competence8);
 
 		$competence9 = new Competence();
 		$competence9->setNom('Composer');
 		$competence9->setNiveau(3);
-		$competence9->setCategory($this->getReference('tech'));
+		$competence9->setCategory($this->getReference('tools'));
 		$manager->persist($competence9);
 
 		$competence10 = new Competence();
 		$competence10->setNom('Wordpress');
 		$competence10->setNiveau(4);
-		$competence10->setCategory($this->getReference('tech'));
+		$competence10->setCategory($this->getReference('tools'));
 		$manager->persist($competence10);
 
 		$competence11 = new Competence();
-		$competence11->setNom('Templating : PHP, Smarty et Twig');
+		$competence11->setNom('Templating : Twig, Smarty et PHP');
 		$competence11->setNiveau(4);
-		$competence11->setCategory($this->getReference('tech'));
+		$competence11->setCategory($this->getReference('front'));
 		$manager->persist($competence11);
 
 		$competence12 = new Competence();
@@ -166,8 +176,38 @@ class LoadCompetenceData extends AbstractFixture implements OrderedFixtureInterf
 		$competence22 = new Competence();
 		$competence22->setNom('API Mailchimp');
 		$competence22->setNiveau(4);
-		$competence22->setCategory($this->getReference('tech'));
+		$competence22->setCategory($this->getReference('back'));
 		$manager->persist($competence22);
+
+		$competence23 = new Competence();
+		$competence23->setNom('NodeJS');
+		$competence23->setNiveau(4);
+		$competence23->setCategory($this->getReference('back'));
+		$manager->persist($competence23);
+
+		$competence24 = new Competence();
+		$competence24->setNom('ElasticSearch');
+		$competence24->setNiveau(4);
+		$competence24->setCategory($this->getReference('back'));
+		$manager->persist($competence24);
+
+		$competence25 = new Competence();
+		$competence25->setNom('RabbitMQ');
+		$competence25->setNiveau(4);
+		$competence25->setCategory($this->getReference('back'));
+		$manager->persist($competence25);
+
+		$competence26 = new Competence();
+		$competence26->setNom('Angular');
+		$competence26->setNiveau(4);
+		$competence26->setCategory($this->getReference('front'));
+		$manager->persist($competence26);
+
+		$competence27 = new Competence();
+		$competence27->setNom('VueJS');
+		$competence27->setNiveau(4);
+		$competence27->setCategory($this->getReference('front'));
+		$manager->persist($competence27);
 
 		$manager->flush();
     }
